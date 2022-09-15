7da9e8b9ee6a2054c1235c9cc2e5edcf3df8cd0c
Franso <francismuasya@gmail.com>
2022-09-15 11:17:49 +0300

    Add - DB client creation func

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,23 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("cannot remove this product from the cart")
+	ErrCantGetItem        = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {}
+
+func RemoveCartItem() {}
+
+func InstantBuy() {}
