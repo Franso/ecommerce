2e82f8e34c3c81ffd51b453742890b3d7e13e840
Franso <francismuasya@gmail.com>
2022-09-16 10:52:39 +0300

    Add - User Sign up

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/franso/ecommerce/database"
+	"github.com/franso/ecommerce/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.UserData(database.Client, "Users")
+var Validate = validator.New()
 
 func HashPassword(password string) string {
 
@@ -10,8 +25,56 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 
 }
 
-func SignUp() gin.Handler {
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// create context
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		// initiate a User object
+		var user models.User
+		// bind the JSON data received to the user object and do a validation
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// validate the data that is bound to the user object and check if the fields meet the requirements
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		// check if the email has already been used by counting the number of documents im Mongo DB with the same email
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		// if the email already exists, return an error
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+
+		}
+
+		// check if the phone number has already been used
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone number is already in use"})
+			return
+		}
+	}
 }
 
 func Login() gin.HandlerFunc {
